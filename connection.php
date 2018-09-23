1092d7e050909fac942ce045742e89a0cb767433
joshuanatan <joshuanatan.jn@gmail.com>
2018-09-23 22:42:19 +0700

    update login

--- connection.php
+++ connection.php
@@ -1,31 +1,30 @@
 <?php
-$servername = $_SERVER["SERVER_NAME"];
-$username = "istallup_jn";
-$password = "gI@ntm0use";
-$dbname = "istallup_istalluph";
- Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
- Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-else echo "Connected successfully";
-?>
-
-<?php
 //$servername = $_SERVER["SERVER_NAME"];
-//$username = "root";
-//$password = "";
+//$username = "istallup_jn";
+//$password = "gI@ntm0use";
 //$dbname = "istallup_istalluph";
 // Create connection
 //$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 //if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+//    die("Connection failed: " . $conn->connect_error);
 //} 
 //else echo "Connected successfully";
+?>
+
+<?php
+$servername = $_SERVER["SERVER_NAME"];
+$username = "root";
+$password = "";
+$dbname = "istallup_istalluph";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+//Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+else echo "Connected successfully";
 
 // THIS COMMENT IS MADE BY NICKY TODAY SHALALALALA
 
--- login/functionSignIn.php
+++ login/functionSignIn.php
@@ -3,29 +3,22 @@ session_start();
 include("../connection.php");
 echo "lolos";
 $sql = "select * from member where email = ? and pass = ?";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param('ss', $email, $pass); //fail disini
-$email = $_POST["email"];
-$pass = $_POST["pass"];
-echo $email;
-echo $pass; 
-if($stmt->execute()){
+if($stmt = $conn->prepare($sql)){
+    $stmt->bind_param('ss', $email, $pass);
+    $email = $_POST["email"];
+    $pass = $_POST["pass"];
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if($row = $result->fetch_assoc()){
+        $_SESSION["idistall"] = $row["id"];
+        $_SESSION["namaistall"] = $row["nama"];
+        header("location:../index.php");
 
+    }
+    else echo "fail";
 }
+else echo " fail";
 
-$result = $stmt->get_result();
-if($row = $result->fetch_assoc()){
-    $_SESSION["idistall"] = $row["nama"];
-    echo $_SESSION["idistall"];
-    echo $row["nama"];
-    header("location:../index.php");
-    die();
-    exit();
-}
-else{
-    header("location:index.php");
-    echo "Wrong Username / Password";
-    die();
-    exit();
-}
+
+
 ?>
